b3ec15b4e65fb71a5effc303b801f9a094e43ad9
Valian <jakub.skalecki@gmail.com>
2015-10-06 22:03:36 +0200

    restructured playbooks and roles

--- vagrant/init.sh
+++ vagrant/init.sh
@@ -1,5 +1,6 @@
 apt-get update
-apt-get install -y git
+apt-get install -y git python-pip
 curl -sSL https://get.docker.com/ | sh
 usermod -aG docker vagrant
+pip install docker-py requests
 shutdown -r now
